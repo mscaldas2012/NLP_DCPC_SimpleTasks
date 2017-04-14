3b8142090c281e342a765f0f8619728b54a7316b
marcelo2012 <mscaldas@gmail.com>
2017-04-14 10:29:29 -0400

    reducing CORS restrictions - allowing calls from anywhere for prototype

--- src/main/java/gov/cdc/nlp/SimpleNlpTasksApplication.java
+++ src/main/java/gov/cdc/nlp/SimpleNlpTasksApplication.java
@@ -2,6 +2,11 @@ package gov.cdc.nlp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @SpringBootApplication
 public class SimpleNlpTasksApplication {
@@ -9,4 +14,14 @@ public class SimpleNlpTasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleNlpTasksApplication.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");//.allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 }
